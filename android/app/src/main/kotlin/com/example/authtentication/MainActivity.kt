5e0d431095d49541d41edb6f6fe9e9c481aae473
Godwin David <oneandtrueblueking@gmail.com>
2023-08-24 10:12:14 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/example/authtentication/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.authtentication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:authtentication/routes/routes.dart';
+import 'package:authtentication/ui/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Efotlex Test',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+      routes: {
+        AppRoutes.login: (context) => const LoginScreen(),
+      },
+    );
+  }
+}
